4c96e90b651753090b1b8de6673b2420e89b7d54
root <root@DESKTOP-ASUSHDV.localdomain>
2026-06-11 12:36:11 +1000

    attempted config file edit to declare canonical

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.decrepitfilth.art',
+                    },
+                ],
+                destination: 'https://decrepitfilth.art/:path*',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
